5c2a10e963a5598acb08ba57e59e8abb6a47d34a
AlfonsoAD <alfonsoad097@gmail.com>
2023-05-30 22:00:00 -0500

    Corrección

--- app/access/changepassword/page.jsx
+++ app/access/changepassword/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import LayoutAcces from "@/components/access/LayoutAcces";
-import FormChangePass from "@/components/access/form-change-pass/form-change-pass";
+import FormChangePass from "@/components/access/form-change-pass/FormChangePass";
 
 const changepassword = () => {
   return (
